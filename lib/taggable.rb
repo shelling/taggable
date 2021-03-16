5dedd3317ff13da80aee3b6b685c32df0ea4700b
Shelling Hsu <navyblueshellingford@gmail.com>
2021-03-16 14:03:55 -0700

    define handler of each rule

--- lib/taggable.rb
+++ lib/taggable.rb
@@ -1,2 +1,34 @@
 require "treetop"
 Treetop.load(File.expand_path("taggable", File.dirname(__FILE__)))
+
+module Taggable
+  class Parser < Treetop::Runtime::CompiledParser
+    include Taggable
+
+    def call(string)
+      if result = parse(string)
+        result.tokenize
+      else
+        raise Exception, self.failure_reason
+      end
+    end
+  end
+
+  class Document < Treetop::Runtime::SyntaxNode
+    def tokenize
+      elements[0].tokenize
+    end
+  end
+
+  class Text < Treetop::Runtime::SyntaxNode
+    def tokenize
+      text_value
+    end
+  end
+
+  class T < Treetop::Runtime::SyntaxNode
+    def tokenize
+      [name.text_value.to_sym, content.elements.map(&:tokenize)]
+    end
+  end
+end
